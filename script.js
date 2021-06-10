a14c58707af3f638b2b9962a9e323adee6ed84f0
Kavish Bamboli <bambolikavish20@gmail.com>
2021-06-10 20:44:29 +0530

    Updated files

--- script.js
+++ script.js
@@ -2,11 +2,10 @@ var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
-var button = document.querySelector("button");
 
 color1.value = "#ff0000";
 color2.value = "#FFFF00";
-css.textContent = "linear-gradient(to-right, red, yellow)";
+css.textContent = "linear-gradient(to right, red, yellow)";
 
 function setGradient() {
 	body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
